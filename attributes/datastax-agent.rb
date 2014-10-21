85cbf13b897b9a3cbb00bdcd1d71ebbeb3c23597
Bill Warner <bill.warner@gmail.com>
2014-10-21 16:07:59 -0700

    add search capibility to find opscenter server

--- attributes/datastax-agent.rb
+++ attributes/datastax-agent.rb
@@ -1,4 +1,6 @@
 default['datastax-agent']['enabled']	   = false 
 default['datastax-agent']['version']       = "4.1.1-1"
 default['datastax-agent']['conf_dir']	   = "/var/lib/datastax-agent/conf"
+default['datastax-agent']['role_based_opscenter'] = false
+default['datastax-agent']['opscenter_role'] = "role:opscenter"
 default['datastax-agent']['opscenter-ip']  = "192.168.32.3"
