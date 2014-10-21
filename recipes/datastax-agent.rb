2e2827ac0dd9e1bfbe20d676a8bdfef824cbd46f
Bill Warner <bill.warner@gmail.com>
2014-10-21 16:09:20 -0700

    Added search logic to find opscenter server

--- recipes/datastax-agent.rb
+++ recipes/datastax-agent.rb
@@ -1,3 +1,20 @@
+# if including this recipe then enable datastax-agent
+node.default['datastax-agent']['enabled'] = true
+
+# search for opscenter role
+if node['datastax-agent']['role_based_opscenter']
+  if Chef::Config[:solo]
+    Chef::Log.warn("This recipe uses search. Chef Solo does not support search.")
+    Chef::Log.warn("Dissable with node['datastax-agent']['role_based_opscenter']")
+  else
+    search(:node, "#{node['opscenter']['role']} AND chef_environment:#{node.chef_environment}") do |m|
+      list = m['ipaddress']
+    end
+  end
+  node.default['datastax-agent']['opscenter-ip'] = list
+end
+
+
 #Install the datastax-agent package
 package "datastax-agent" do
   version node['datastax-agent']['version']
